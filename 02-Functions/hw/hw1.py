6444b0f0fcbd85992277e11f2fdce7898a070642
Кирилл Ожигин <cuprum.oxigen@gmail.com>
2019-12-21 02:47:38 +0300

    I have updated 02 functions

--- 02-Functions/hw/hw1.py
+++ 02-Functions/hw/hw1.py
@@ -3,8 +3,6 @@ characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
               'o', 'p', 'q', 'r', 's', 't', 'u',
               'v', 'w', 'x', 'y', 'z']
 
-numeral = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-
 
 def letters_range(*args, **gloss) -> None:
     """ The function executes range function for letters
