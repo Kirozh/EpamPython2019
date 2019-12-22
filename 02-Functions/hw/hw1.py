5f8d584dee6a1b9ac969492e19c89aeabf1e4a48
Кирилл Ожигин <cuprum.oxigen@gmail.com>
2019-12-22 21:52:30 +0300

    I have updated 02 functions

--- 02-Functions/hw/hw1.py
+++ 02-Functions/hw/hw1.py
@@ -9,11 +9,9 @@ def letters_range(*args, **gloss) -> None:
 
     :param args: input *args
     :param gloss: input **kwards
-    :return:
+    :return: None
     """
-    start = 0
-    finish = 0
-    step = 1
+
 
     def return_list(start, finish, step=1, **gloss):
         temp_list = []
@@ -30,18 +28,20 @@ def letters_range(*args, **gloss) -> None:
                     temp_list.append(gloss[characters[i]])
         return temp_list
 
-    if len(args) == 1:
 
-        if args[0] not in numeral:  # if only-arguement is finish
+    # starting function letters_range
+    if len(args) == 1:  # if the only argument got
+
+        if type(args[0]) is not int:  # if only-arguement is finish
             start = 0
             finish = characters.index(args[0])
             print(return_list(start, finish))
-        elif args[0] in numeral:
+        elif type(args[0]) is int:  # if only argument is numeral
             print('Error: The only argument must be letter, not numeral')
         else:
             print('Error: The only argument must be letter')
-    elif len(args) == 2:
 
+    elif len(args) == 2:  # if 2 arguments are given
         if gloss == {}:  # if 2 args are start and finish
             if args[0] in characters and args[1] in characters and \
                     characters.index(args[0]) <= characters.index(args[1]):
@@ -49,7 +49,7 @@ def letters_range(*args, **gloss) -> None:
                 start = characters.index(args[0])
                 finish = characters.index(args[1])
                 print(return_list(start, finish))
-            elif args[0] in characters and type(args[1]) is int:
+            elif args[0] in characters and type(args[1]) is int:  # if first arg is finish letter and second arg is step
                 start = 0
                 finish = characters.index(args[0])
                 step = args[1]
@@ -58,40 +58,40 @@ def letters_range(*args, **gloss) -> None:
                 print('Error: Arguments must be letter, not numeral')
             elif characters.index(args[0]) > characters.index(args[1]):
                 print('Error: Wrong letter order')
-        else:
-
-            if args[0] in characters and args[1] in characters:
-                # for i in range(characters.index(args[0]), characters.index(args[1]), 1):
-                #     if gloss.get(characters[i]) is None:
-                #         temp.append(characters[i])
-                #     else:
-                #         temp.append(gloss[characters[i]])
-                # print(temp)
-
+        else:  # if **kwards is given
+            if args[0] in characters and args[1] in characters:  # if args are letters
                 start = characters.index(args[0])
                 finish = characters.index(args[1])
-                step = 1
                 print(return_list(start, finish, **gloss))
+
     elif len(args) == 3:
 
         if args[0] in characters and args[1] in characters:
-            if type(args[2]) is int:
-                start = characters.index(args[0])
-                finish = characters.index(args[1])
-                step = args[2]
-                print(return_list(start, finish, step))
+            if (characters.index(args[0]) < characters.index(args[1]) and int(args[2]) > 0) \
+                    or \
+                    (characters.index(args[1]) < characters.index(args[0]) and int(args[2]) < 0):
+                if type(args[2]) is int:
+                    start = characters.index(args[0])
+                    finish = characters.index(args[1])
+                    step = args[2]
+                    print(return_list(start, finish, step))
+                else:
+                    print('Error: Third argument must be numeral!')
             else:
-                print('Error: Third arguement must be numeral!')
+                print('Error: Wrong letter order')
         else:
-            print('Error: Arguements must be letters!')
+            print('Error: Arguments must be letters!')
     elif len(args) > 3:
         print('Error: Extra args!')
     elif len(args) == 0:
         print('Error: No input args')
 
 
+'''
+Main part
+'''
 print('range(b, w, 2)')
-letters_range('bb', 'w', 2)
+letters_range('b', 'w', 2)
 print('range(g)')
 letters_range('g')
 print('range(g, p')
